6c6827a4f7316c8aa1afa03f7262f52f76fcaf95
Raymond Llata <raymond.llata@gmail.com>
2023-07-15 13:58:17 -0700

    Finalized Data Collection UI
    Added Sequence function to install three-stage data collection process using Global Variables

--- main.py
+++ main.py
@@ -3,11 +3,18 @@ import time
 import threading
 import random
 
+PERSON_ONE_DATA = []
+PERSON_TWO_DATA = []
+MYSTERY_PERSON_DATA = []
+PERSON_TRACKER = 0
+
+
 #Class for GUI
 class dataCollectionGUI:
 
     #basic tKinter GUI, can be updated later
     def __init__(self):
+
         self.root = tk.Tk()
         self.root.title = "Biometric Keystroke Detector"
         self.root.geometry = "800x600"
@@ -22,9 +29,12 @@ class dataCollectionGUI:
         self.input_entry = tk.Entry(self.frame, width=40, font=("Helvetica", 24))
         self.input_entry.grid(row=1, column=0, columnspan=2, padx=5, pady=10)
         #inputing text starts the GUI
-        self.input_entry.bind("<KeyPress>", self.start)
+        self.input_entry.bind("<KeyPress>", self.sequence)
 
         #Can add similarity percentage to each person as a real-time feature
+
+        self.time_label = tk.Label(self.frame, text="Time: 0.00s")
+        self.time_label.grid(row=2, column=0, columnspan=2, padx=5, pady=10)
 
         #implementing reset button
         self.reset_button = tk.Button(self.frame, text="Reset", command=self.reset)
@@ -37,25 +47,67 @@ class dataCollectionGUI:
 
         self.root.mainloop()
 
+
+    def sequence(self, event):
+        if PERSON_TRACKER == 0:
+            self.sample_label.config(text="Person A Type Now!")
+            PERSON_ONE_DATA.append((event.keycode, self.counter))
+        if PERSON_TRACKER == 1:
+            self.sample_label.config(text="Person B Type Now!")
+            PERSON_TWO_DATA.append((event.keycode, self.counter))
+        if PERSON_TRACKER == 2:
+            self.sample_label.config(text="Mystery Person Type Now!")
+            MYSTERY_PERSON_DATA.append((event.keycode, self.counter))
+
+        if not self.started:
+            self.start(event)
+        if self.counter >= 5:
+            self.started = False
+            self.clearSequence()
+            print(PERSON_ONE_DATA)
+
+
+
     #start button logic
     def start(self, event):
-        if not self.started:
-            #keycodes for shift, alt, ctrl via Google
-            if not event.keycode in [16, 17, 18]:
-                t = threading.Thread(target=self.time_thread)
-                t.start()
+        #keycodes for shift, alt, ctrl via Google
+        if not event.keycode in [16, 17, 18]:
+            self.started = True
+            t = threading.Thread(target=self.timeThreading)
+            t.start()
             #need to add logic to stop after 15 seconds
+
 
 
     #adding timing functionality that runs on a different thread then GUI
     #will need to add data collection here
     def timeThreading(self):
-        while self.started:
+        while self.started and self.counter < 5.0:
             time.sleep(0.1)
             self.counter += 0.1
 
+            timer = self.counter
+            self.time_label.config(text=f"Time: {timer:.2f} Seconds")
+        print("data:" + str(PERSON_ONE_DATA))
+        self.clearSequence()
+
+    def clearSequence(self):
+        self.input_entry.delete(0, tk.END)
+        self.counter = 0
+        self.started = False
+        global PERSON_TRACKER
+        PERSON_TRACKER += 1
+        if PERSON_TRACKER < 3:
+            self.sample_label.config(text="Next Person Type Whenever!")
+        else:
+            self.sample_label.config(text="Well Done! Computing Probabilities Now")
+            #need to import computing logic here
+            print(PERSON_ONE_DATA)
+            print(PERSON_TWO_DATA)
+            print(MYSTERY_PERSON_DATA)
 
 
+    #reset test function
     def reset(self):
         pass
 
